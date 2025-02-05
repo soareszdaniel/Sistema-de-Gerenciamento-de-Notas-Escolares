e0d40a01860705b449e7d68addfe0738321e98a3
Daniel Soares <soares.dandansss@gmail.com>
2025-02-05 14:02:36 -0300

    Criei o bd

--- Sistema-de-Gerenciamento-de-Notas-Escolares/src/Main.java
+++ Sistema-de-Gerenciamento-de-Notas-Escolares/src/Main.java
@@ -1,10 +1,13 @@
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("Driver do banco de dados não encontrado");
-        }
+        ConsultaSQL consulta = new ConsultaSQL();
+        consulta.estabeleregarConexao();
+
+        consulta.adicionarAluno("Alfedro", 30, 1250);
+        consulta.consultarNome();
+
 
     }
 }
